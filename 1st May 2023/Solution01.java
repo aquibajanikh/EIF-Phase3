2aa8825004006204f3da5c7be2cbd7a7b146a410
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-05-01 11:44:54 +0400

    Create Solution01.java

---
+++ 1st May 2023/Solution01.java
@@ -0,0 +1,12 @@
+// Write a Java program to display the following :     (name) lives in (city) and works at (bank/company). He/She spends (amount) every month and is (age) years old.
+
+public class Challenge01{
+    public static void main(String args[]){
+        String city = "Dubai";
+        String name = "Jane";
+        String company = "DXB";
+        double amount = 6000.9;
+        int age = 35;
+        System.out.println(name + " lives in "+ city + " and works at "+ company+". He/She spends " + (amount) +" every month and is "+ age +" years old.");
+    }
+}
