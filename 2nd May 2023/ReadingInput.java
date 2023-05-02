564daaf20ccf5dde98a438e84fe6b3a508d676ad
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-05-02 11:33:16 +0400

    Create ReadingInput.java

---
+++ 2nd May 2023/ReadingInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class LoopsJava{
+    public static void main(String args[]){
+       Scanner input = new Scanner(System.in);
+       int value = input.nextInt();
+       System.out.print("You entered : "+value);
+    }
+}
