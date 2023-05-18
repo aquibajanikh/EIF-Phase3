ad599d78073c3295885ef6a3e70d5885a5490323
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-05-18 13:21:12 +0530

    Update SpringBootJdbcController.java

--- JDBC with Spring/src/main/java/com/example/demo/SpringBootJdbcController.java
+++ JDBC with Spring/src/main/java/com/example/demo/SpringBootJdbcController.java
@@ -1,15 +1,21 @@
-package com.example.demo;  
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.beans.factory.annotation.Autowired;  
-import org.springframework.jdbc.core.JdbcTemplate;  
-import org.springframework.web.bind.annotation.RestController;  
-@RestController  
-public class SpringBootJdbcController {  
-    @Autowired  
-    JdbcTemplate jdbc;    
-    @RequestMapping("/insert")  
-    public String index(){  
-        jdbc.execute("insert into user(name,email)values('javatpoint','java@javatpoint.com')");  
-        return"data inserted Successfully";  
-    }  
+package com.example.review_items;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootJdbcController {
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @RequestMapping("/insert")
+    public String index(){
+        jdbc.execute("INSERT INTO `sys`.`all_items` (`item_id`, `item_title`, `item_price`) VALUES ('5', 'Narnia', '780');");
+        return "Insertion successful";
+    }
 }
+
+
+// http://localhost:8080/
