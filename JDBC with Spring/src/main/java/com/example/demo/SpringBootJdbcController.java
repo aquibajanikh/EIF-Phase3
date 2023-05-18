4e2763e8b19ad1c059173e2dbdab91c34b5744c8
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-05-18 11:31:54 +0530

    Add files via upload

---
+++ JDBC with Spring/src/main/java/com/example/demo/SpringBootJdbcController.java
@@ -0,0 +1,15 @@
+package com.example.demo;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.jdbc.core.JdbcTemplate;  
+import org.springframework.web.bind.annotation.RestController;  
+@RestController  
+public class SpringBootJdbcController {  
+    @Autowired  
+    JdbcTemplate jdbc;    
+    @RequestMapping("/insert")  
+    public String index(){  
+        jdbc.execute("insert into user(name,email)values('javatpoint','java@javatpoint.com')");  
+        return"data inserted Successfully";  
+    }  
+}
