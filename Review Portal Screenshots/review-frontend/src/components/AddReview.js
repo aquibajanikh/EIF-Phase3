656b22ab244def5fdb57f83f23987d17b2c1721f
aquibajanikh <123448104+aquibajanikh@users.noreply.github.com>
2023-05-22 12:16:41 +0530

    Add files via upload

--- Review Portal Screenshots/review-frontend/src/components/AddReview.js
+++ Review Portal Screenshots/review-frontend/src/components/AddReview.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 function AddReview(){
 
@@ -8,18 +9,19 @@ function AddReview(){
 
     return(
     <section>
-        <form>
+        <form onSubmit={(e)=>{e.preventDefault();
+            axios.post("http://localhost:3000/",{username, reviewContent, reviewProduct})}}>
             <div>
                 <label htmlFor="username">Name : </label>
                 <input value={username} onChange={(e)=>{setUsername(e.target.value); console.log(username);}} id="username" type="text" placeholder="Your name here"/>
             </div>
             <div>
                 <label htmlFor="reviewContent">Review : </label>
-                <input id="reviewContent" type="text" placeholder="Your review here"/>
+                <input value={reviewContent} onChange={(e)=>{setReviewContent(e.target.value); console.log(reviewContent);}} id="reviewContent" type="text" placeholder="Your review here"/>
             </div>
             <div>
                 <label htmlFor="reviewProduct">Product : </label>
-                <input id="reviewProduct" type="text" placeholder="Product name here"/>
+                <input value={reviewProduct} onChange={(e)=>{setReviewProduct(e.target.value); console.log(reviewProduct);}} id="reviewProduct" type="text" placeholder="Product name here"/>
             </div>
             <input type="submit" value="Submit Review"/>
         </form>
